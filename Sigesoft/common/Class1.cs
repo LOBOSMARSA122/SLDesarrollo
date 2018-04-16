27e05e04d9f8301361f246a170cd682d90843a0f
amerchan33 <albertomerchanc@hotmail.com>
2018-04-16 09:51:06 -0500

    otros

---
+++ Sigesoft/common/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sigesoft.Common
+{
+    class Class1
+    {
+
+        public string Type { get; set; }
+    }
+}
